43ba217d065c53d2b7d8d3ffb9b1b4875e15c23c
William Wnekowicz <william.wnekowicz@gmail.com>
2017-11-15 23:57:28 -0800

    Add prod build config

---
+++ tools/webpack.config.prd.js
@@ -0,0 +1,13 @@
+var webpack = require('webpack');
+var baseConfig = require('./webpack.config.base.js');
+
+var config = Object.create(baseConfig);
+config.devtool = 'source-map';
+config.plugins = [
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  }),
+];
+
+module.exports = config;
