ffaa8c87a2edcde6ebc6786d0f1f90e9677cdb18
Teon L Brooks <teon.brooks@gmail.com>
2017-11-18 13:45:31 -0800

    update build

--- dist/experiment.js
+++ dist/experiment.js
@@ -59,6 +59,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
+	exports.Experiment = exports.jsPsych = undefined;
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
@@ -137,7 +138,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	  return Experiment;
 	}(_react.Component);
 	
-	exports.default = Experiment;
+	exports.jsPsych = _jspsych2.default;
+	exports.Experiment = Experiment;
 
 /***/ }),
 /* 1 */
